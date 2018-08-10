f7d38a74ca1b425fcebb8c01c2ef575ca0dc7b32
Aleksandr Grebnev <aleksandr.grebnev@syncopate.ru>
2018-08-10 11:49:24 +0400

    SFC-8 Скрываем хэдер и футер в режиме превью

--- assets/js/components/layout/Footer.vue
+++ assets/js/components/layout/Footer.vue
@@ -1,11 +1,30 @@
 <template>
-    <footer class="b-page__footer">
+    <footer class="b-page__footer" v-if="this.isStorefrontPreview == false">
         Это футер
     </footer>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: 'FooterComponent'
+        name: 'FooterComponent',
+        data() {
+            return {
+                isStorefrontPreview: false
+            }
+        },
+        watch: {
+            storefrontPreview: {
+                handler: function (status, old) {
+                    this.isStorefrontPreview = status;
+                    console.log(status);
+                },
+                deep: true
+            }
+        },
+        computed: {
+            ...mapGetters(['storefrontPreview'])
+        }
     }
 </script>
---
+++ assets/js/store/mutations.js
@@ -0,0 +1,5 @@
+export default {
+    storefrontPreview (state, status) {
+        state.storefrontPreview = status;
+    }
+}
