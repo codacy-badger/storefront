c8a9c115117d903fa7955687f45ff1b49320d46b
Aleksandr Grebnev <aleksandr.grebnev@syncopate.ru>
2018-08-21 14:43:45 +0400

    SFC-21 add plugin logotype block

--- assets/js/plugins/gjs-logo/blocks.js
+++ assets/js/plugins/gjs-logo/blocks.js
@@ -5,7 +5,6 @@ export default (editor, opt = {}) => {
     const bm = editor.BlockManager;
     const logoPfx = c['logoClsPfx'] || cLogoRef;
     const style = c.defaultStyle ? `
-      <style type="text/css">
         .${logoPfx} {
             width: 360px;
             height: auto;
@@ -13,15 +12,51 @@ export default (editor, opt = {}) => {
             min-height: 40px;
             margin: 20px;
         }
-      </style>
   ` : '';
+
+    const attrsToString = attrs => {
+        const result = [];
+
+        for (let key in attrs) {
+            let value = attrs[key];
+            const toParse = value instanceof Array || value instanceof Object;
+            value = toParse ? JSON.stringify(value) : value;
+            result.push(`${key}=${toParse ? `'${value}'` : `"${value}"`}`);
+        }
+
+        return result.length ? ` ${result.join(' ')}` : '';
+    }
+
+
+    const keyWidth = 'width';
+    const step = 0.2;
+    const minDim = 1;
+    const currentUnit = 1;
+    const resizerBtm = { tl: 0, tc: 0, tr: 0, cl: 0, cr:0, bl:0, br: 0, minDim };
+    const resizerRight = { ...resizerBtm, cr: 1, bc: 0, keyWidth, currentUnit, minDim, step };
+
+    const logoAttr = {
+        class: logoPfx,
+        'data-gjs-type': 'gjs-logo',
+        'data-gjs-resizable': resizerRight,
+        'data-gjs-custom-name': logoRef,
+        'data-gjs-unstylable': ['width'],
+        'src': 'https://gn266.cdn.gamenet.ru/TY0Xv2riHu/6hxyU/o_1DNqc3.png'
+    };
+    const attrsLogo = attrsToString(logoAttr);
+
 
     if (c.blocks.indexOf(cLogoRef) >= 0) {
         bm.add(cLogoRef, {
             label: c['labelLogoBlock'],
             category: c['labelLogoCategory'],
             attributes: { class: 'fa fa-square' },
-            content: `<img class="${logoPfx}"  src="https://gn266.cdn.gamenet.ru/TY0Xv2riHu/6hxyU/o_1DNqc3.png" data-gjs-type="gjs-logo"></img>${style}`,
+            content: `
+                <img ${attrsLogo} ></img>
+                <style>
+                    ${style}
+                </style>
+            `,
         });
     }
 }
