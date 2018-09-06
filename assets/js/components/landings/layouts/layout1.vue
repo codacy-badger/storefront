245fcc04c5d565327d537e82bd5d134f6dbc633a
Dmitriy Sinichkin <dmitriy.sinichkin@gmail.com>
2018-09-06 10:34:50 +0300

    SFC-0 background styler

--- assets/js/components/landings/layouts/layout1.vue
+++ assets/js/components/landings/layouts/layout1.vue
@@ -1,102 +1,113 @@
 <template>
-  <section class="b-layout-1" v-styler:section="$sectionData.classes" :class="$sectionData.classes">
+    <section class="b-layout-1" v-styler:section="$sectionData.classes" :class="$sectionData.classes"
+             v-styler="$sectionData.styles[0]">
+        <div class="b-layout-1__wrap">
+            <div class="b-layout-1__content">
+                <div class="b-layout-1__panel">
+                    <div class="b-layout-1__panel__wrap">
+                        <div class="b-layout-1__panel__section">
+                            <uploader class="b-layout-1__panel__logo-img" path="$sectionData.logos[0].path"
+                                      :title="$sectionData.logos[0].alt" :alt="$sectionData.logos[0].alt"></uploader>
+                        </div>
+                        <div class="b-layout-1__panel__section">
+                            <div class="b-layout-1__panel__title" v-styler="$sectionData.slogan[0].text"
+                                 v-text="$sectionData.slogan[0].title"></div>
+                            <div class="b-layout-1__panel__title panel__title_big"
+                                 v-styler="$sectionData.slogan[1].text" v-text="$sectionData.slogan[1].title"></div>
+                        </div>
 
-          <div class="b-layout-1__wrap">
-              <div class="b-layout-1__content">
-                  <div class="b-layout-1__panel">
-                      <div class="b-layout-1__panel__wrap">
-                          <div class="b-layout-1__panel__section">
-                              <uploader class="b-layout-1__panel__logo-img" path="$sectionData.logos[0].path" :title="$sectionData.logos[0].alt" :alt="$sectionData.logos[0].alt"></uploader>
-                          </div>
-                          <div class="b-layout-1__panel__section">
-                              <div class="b-layout-1__panel__title" v-styler="$sectionData.slogan[0].text" v-text="$sectionData.slogan[0].title"></div>
-                              <div class="b-layout-1__panel__title panel__title_big" v-styler="$sectionData.slogan[1].text" v-text="$sectionData.slogan[1].title"></div>
-                          </div>
-
-                          <div class="b-layout-1__panel__section">
-                              <ul class="b-layout-1__panel__features">
-                                  <div class="b-layout-1__panel__feature" v-for="(item, index) in $sectionData.features">
-                                      <span  v-text="item.title"  v-styler="item.text"></span>
-                                  </div>
-                              </ul>
-                          </div>
-                          <div class="b-layout-1__panel__section b-layout-1__panel__section_grow">
-                            <span class="b-layout-1__b-button" @click.prevent="onClick" :class="$sectionData.buttons[0].classes" :href="$sectionData.buttons[0].href" v-html="$sectionData.buttons[0].text" v-styler="$sectionData.buttons[0].button">
+                        <div class="b-layout-1__panel__section">
+                            <ul class="b-layout-1__panel__features">
+                                <div class="b-layout-1__panel__feature" v-for="(item, index) in $sectionData.features">
+                                    <span v-text="item.title" v-styler="item.text"></span>
+                                </div>
+                            </ul>
+                        </div>
+                        <div class="b-layout-1__panel__section b-layout-1__panel__section_grow">
+                            <span class="b-layout-1__b-button" @click.prevent="onClick"
+                                  :class="$sectionData.buttons[0].classes" :href="$sectionData.buttons[0].href"
+                                  v-html="$sectionData.buttons[0].text" v-styler="$sectionData.buttons[0].button">
 
                             </span>
-                          </div>
-                          <div class="b-layout-1__panel__section">
-                              <footer class="b-layout-1__footer">
-                                  <div class="b-layout-1__footer__group">
-                                      <uploader class="b-layout-1__footer__el b-layout-1__footer__el_logo" path="$sectionData.logos[1].path" :title="$sectionData.logos[1].alt" :alt="$sectionData.logos[1].alt"></uploader>
-                                      <span class="b-layout-1__footer__el">
-                                          <span v-text="$sectionData.copyright.title" v-styler="$sectionData.copyright.text">
+                        </div>
+                        <div class="b-layout-1__panel__section">
+                            <footer class="b-layout-1__footer">
+                                <div class="b-layout-1__footer__group">
+                                    <uploader class="b-layout-1__footer__el b-layout-1__footer__el_logo"
+                                              path="$sectionData.logos[1].path" :title="$sectionData.logos[1].alt"
+                                              :alt="$sectionData.logos[1].alt"></uploader>
+                                    <span class="b-layout-1__footer__el">
+                                          <span v-text="$sectionData.copyright.title"
+                                                v-styler="$sectionData.copyright.text">
                                               2018-2018 gamesite.com
                                           </span>
                                       </span>
-                                  </div>
-                                  <div class="b-layout-1__footer__group b-layout-1__footer__group_light">
+                                </div>
+                                <div class="b-layout-1__footer__group b-layout-1__footer__group_light">
                                       <span v-for="item in $sectionData.links">
 
                                           <a @click.prevent="openLink(item)" :class="item.classes"
-                                                class="b-layout-1__footer__el b-layout-1__footer__el_link" :href="item.href" v-html="item.text"  v-styler="item">
+                                             class="b-layout-1__footer__el b-layout-1__footer__el_link"
+                                             :href="item.href" v-html="item.text" v-styler="item">
                                           </a>
                                       </span>
-                                  </div>
-                              </footer>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="b-layout-1__b-18plus" v-text="$sectionData.age.title" v-styler="$sectionData.age.text">
-              18+
-          </div>
-
+                                </div>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="b-layout-1__b-18plus" v-text="$sectionData.age.title" v-styler="$sectionData.age.text">
+            18+
+        </div>
     </section>
-
-
 </template>
 
 <script>
     import * as types from './../../../plugins/Vuse/types';
 
     export default {
-      name: 'Layout1',
-      cover: 'img/covers/layout1.png',
-      group: 'Layouts',
-      $schema: {
-        classes: types.ClassList,
-        age: {
-            title: '18+',
-            text: types.Text
-        },
-        copyright: {
-            title: '2018-2018 gamesite.com',
-            text: types.Text
-        },
-        buttons: [
-           {
-               text: 'Play Now',
-               href: "#",
-               button: types.Button,
-           },
-        ],
-        features: [
-            {
-                title: 'Modern 3D graphics in the browser',
-                text: types.Text,
+        name: 'Layout1',
+        cover: 'img/covers/layout1.png',
+        group: 'Layouts',
+        $schema: {
+            classes: types.ClassList,
+            styles: [
+                {
+                    text: types.StyleList
+                }
+            ],
+            age: {
+                title: '18+',
+                text: types.Text
             },
-            {
-                title: 'Create multiple characters on the server',
-                text: types.Text,
+            copyright: {
+                title: '2018-2018 gamesite.com',
+                text: types.Text
             },
-            {
-                title: 'Wide opportunities for pumping the hero',
-                text: types.Text,
-            },
-        ],
-        slogan: [
+            buttons: [
+                {
+                    text: 'Play Now',
+                    href: "#",
+                    button: types.Button,
+                },
+            ],
+            features: [
+                {
+                    title: 'Modern 3D graphics in the browser',
+                    text: types.Text,
+                },
+                {
+                    title: 'Create multiple characters on the server',
+                    text: types.Text,
+                },
+                {
+                    title: 'Wide opportunities for pumping the hero',
+                    text: types.Text,
+                },
+            ],
+            slogan: [
                 {
                     title: 'CaLL',
                     text: types.Title,
@@ -105,62 +116,62 @@
                     title: 'to action!',
                     text: types.Title,
                 },
-        ],
-        logos: [
-            {
-                path: 'https://gn616.cdn.gamenet.ru/TY0Xv2riHu/6nOTs/o_1My2mo.png',
-                alt: 'Default Logo',
-                logo: types.Logo,
-            },
-            {
-                path: 'https://gn839.cdn.gamenet.ru/TY0Xv2riHu/6nOU5/o_21POBx.png',
-                alt: 'Default Logo',
-                logo: types.Logo,
-            },
-        ],
-        links: [
-            {
-                text: 'Catalog of games',
-                href: '#',
-                button: types.Button,
-            },
-            {
-                text: 'Support',
-                href: '#',
-                button: types.Button,
-            },
-            {
-                text: 'Rules',
-                href: '#',
-                button: types.Button,
-            },
-        ],
-      },
-      props: {
-        id: {
-          type: Number, required: true
-        }
-      },
-      methods: {
-          onClick () {
-               if (this.$builder.isEditing) return;
+            ],
+            logos: [
+                {
+                    path: 'https://gn616.cdn.gamenet.ru/TY0Xv2riHu/6nOTs/o_1My2mo.png',
+                    alt: 'Default Logo',
+                    logo: types.Logo,
+                },
+                {
+                    path: 'https://gn839.cdn.gamenet.ru/TY0Xv2riHu/6nOU5/o_21POBx.png',
+                    alt: 'Default Logo',
+                    logo: types.Logo,
+                },
+            ],
+            links: [
+                {
+                    text: 'Catalog of games',
+                    href: '#',
+                    button: types.Button,
+                },
+                {
+                    text: 'Support',
+                    href: '#',
+                    button: types.Button,
+                },
+                {
+                    text: 'Rules',
+                    href: '#',
+                    button: types.Button,
+                },
+            ],
+        },
+        props: {
+            id: {
+                type: Number, required: true
+            }
+        },
+        methods: {
+            onClick() {
+                if (this.$builder.isEditing) return;
 
-               const endpoint = this.$sectionData.buttons[0].href;
-          },
-          openLink (el) {
-              if (this.$builder.isEditing) return;
+                const endpoint = this.$sectionData.buttons[0].href;
+            },
+            openLink(el) {
+                if (this.$builder.isEditing) return;
 
-              if (!el) {
-                  return;
-              }
+                if (!el) {
+                    return;
+                }
 
-              const href = el.href;
+                const href = el.href;
 
-              if (el && undefined !== href ) {
-                  window.open(href);
-              }
+                if (el && undefined !== href) {
+                    window.open(href);
+                }
 
-          }
+            }
         }
     };
 </script>
