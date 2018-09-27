67b07b65e6a6ef5069086d53f2abcc93a2fdfb80
Aleksandr Grebnev <aleksandr.grebnev@syncopate.ru>
2018-09-27 17:12:37 +0400

    SFC-38 added componet gallery

--- src/cscripts/gallery3.js
+++ src/cscripts/gallery3.js
@@ -1,18 +1,18 @@
 ;
-export function galleryTwo() {
-  const TARGET_LINK = 'gallery-three-link';
-  const TARGET_URl = 'gallery-three-url';
-  const TARGET_POPUP = 'gallery-three-popup';
-  const TARGET_POPUP_CONTENT = 'gallery-three-popup-content';
-  const TARGET_POPUP_CLOSE = 'gallery-three-popup-close';
+export function galleryThree() {
+  const TARGET_GALLERY_THREE_LINK = 'gallery-three-link';
+  const TARGET_GALLERY_THREE_URl = 'gallery-three-url';
+  const TARGET_GALLERY_THREE_POPUP = 'gallery-three-popup';
+  const TARGET_GALLERY_THREE_POPUP_CONTENT = 'gallery-three-popup-content';
+  const TARGET_GALLERY_THREE_POPUP_CLOSE = 'gallery-three-popup-close';
 
-  var links = document.querySelectorAll('['+ TARGET_LINK +']');
-  var popup = document.querySelectorAll('['+ TARGET_POPUP +']')[0];
-  var popupC = document.querySelectorAll('['+ TARGET_POPUP_CONTENT +']')[0];
-  var popupX = document.querySelectorAll('['+ TARGET_POPUP_CLOSE +']')[0];
+  var linksG3 = document.querySelectorAll('['+ TARGET_GALLERY_THREE_LINK +']');
+  var popup = document.querySelectorAll('['+ TARGET_GALLERY_THREE_POPUP +']')[0];
+  var popupC = document.querySelectorAll('['+ TARGET_GALLERY_THREE_POPUP_CONTENT +']')[0];
+  var popupX = document.querySelectorAll('['+ TARGET_GALLERY_THREE_POPUP_CLOSE +']')[0];
   var defUrl = 'https://www.youtube.com/embed/dqHeutdSSyM';
 
-    [].forEach.call(links, function(el, i) {
+    [].forEach.call(linksG3, function(el, i) {
         el.onclick = function(e) {
             clickLink(el);
         };
@@ -29,13 +29,13 @@ export function galleryTwo() {
     function clickLink (el) {
         var content = '';
         let m = false;
-        var href = el.getAttribute(TARGET_URl);
+        var href = el.getAttribute(TARGET_GALLERY_THREE_URl);
         var url = href !== '' ? href : defUrl;
         m = matchYoutubeUrl(url);
         if (m) {
-            content = '<iframe id="content"  width="100%" height="100%" src="https://www.youtube.com/embed/' + m + '?rel=0&amp;wmode=transparent&amp;autoplay=1&amp;enablejsapi=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>'
+            content = '<iframe id="contentGalleryThreePopup"  width="100%" height="100%" src="https://www.youtube.com/embed/' + m + '?rel=0&amp;wmode=transparent&amp;autoplay=1&amp;enablejsapi=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>'
         } else {
-            content = '<img id="content" width="100%"  height="100%" src="' + url + '"></img>'
+            content = '<img id="contentGalleryThreePopup" width="100%"  height="100%" src="' + url + '"></img>'
         }
         setTimeout(opepPopup (content), 500);
     }
@@ -43,7 +43,7 @@ export function galleryTwo() {
     function opepPopup (c) {
         popupC.innerHTML = c;
         popup.style.display = "flex";
-        var c = document.getElementById('content');
+        var c = document.getElementById('contentGalleryThreePopup');
         var actualWidth = c.clientWidth;
         var calcHeight = actualWidth * 0.5625;
         c.style.height =  calcHeight + 'px';
@@ -63,6 +63,6 @@ export function galleryTwo() {
         return false
     }
 
-}
+};
 
-galleryTwo();
+galleryThree();
