f0c8dca12393c60953add7977d5ccb8be68a041a
Aleksandr Grebnev <aleksandr.grebnev@syncopate.ru>
2018-09-28 16:31:18 +0400

    SFC-39 add slices titles, changed component gallery with popup

---
+++ src/components/landings/titles/title1.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="b-title-one" v-styler:section="$sectionData.mainStyle" :class="$sectionData.mainStyle.classes" v-bind:style="$sectionData.mainStyle.styles">
+      <h1 class="b-title-one__title" v-styler="$sectionData.title" v-html="$sectionData.title"></h1>
+    </section>
+</template>
+
+<script>
+import * as types from '@plugins/Vuse/types'
+
+export default {
+  name: 'Title1',
+  cover: '',
+  group: 'titles',
+  $schema: {
+    mainStyle: types.StyleObject,
+    title: types.Title
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.b-title-one
+  color: #000
+  font-family: Helvetica, Arial, sans-serif
+  width: 100%
+  position: relative
+  text-align: center
+  display: flex
+  align-items: center
+  height: 10rem
+  &.is-editable
+    resize: vertical
+    overflow: hidden
+  &__title
+    width: 100%
+    font-size: 4rem
+    display: block
+  &::selection, & ::selection
+    color: #ff0
+    background: #000
+</style>
---
+++ src/components/landings/titles/title2.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="b-title-two" v-styler:section="$sectionData.mainStyle" :class="$sectionData.mainStyle.classes" v-bind:style="$sectionData.mainStyle.styles">
+      <h2 class="b-title-two__title" v-styler="$sectionData.title" v-html="$sectionData.title"></h2>
+    </section>
+</template>
+
+<script>
+import * as types from '@plugins/Vuse/types'
+
+export default {
+  name: 'Title2',
+  cover: '',
+  group: 'titles',
+  $schema: {
+    mainStyle: types.StyleObject,
+    title: types.Title
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.b-title-two
+  color: #000
+  font-family: Helvetica, Arial, sans-serif
+  width: 100%
+  position: relative
+  text-align: center
+  display: flex
+  align-items: center
+  height: 5rem
+  &.is-editable
+    resize: vertical
+    overflow: hidden
+  &__title
+    width: 100%
+    font-size: 2rem
+    display: block
+  &::selection, & ::selection
+    color: #ff0
+    background: #000
+</style>
