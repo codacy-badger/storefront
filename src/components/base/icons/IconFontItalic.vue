37153d81a933c3fc19e50dcd94cb1bc9b2ff87dc
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2018-11-21 02:29:03 +0300

    PTH-81 moves svg tag and viewBox attribute from BaseIcon to actual icons components

--- src/components/base/icons/IconFontItalic.vue
+++ src/components/base/icons/IconFontItalic.vue
@@ -1,3 +1,5 @@
 <template>
-  <path d="M4 0V3H6.21L2.79 11H0V14H8V11H5.79L9.21 3H12V0H4Z"/>
+  <svg viewBox="0 0 12 14">
+    <path d="M4 0V3H6.21L2.79 11H0V14H8V11H5.79L9.21 3H12V0H4Z"/>
+  </svg>
 </template>
