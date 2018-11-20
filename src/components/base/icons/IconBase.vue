528a88d8253e0b948386ec4ff253838294b572c4
Alex Shabalin <temarie@ya.ru>
2018-11-20 17:58:20 +0500

    Slot 2 Dynamic components in Icons

--- src/components/base/icons/IconBase.vue
+++ src/components/base/icons/IconBase.vue
@@ -2,13 +2,12 @@
   <svg xmlns="http://www.w3.org/2000/svg"
        :width="width"
        :height="height"
-       :viewBox="viewBox"
        :aria-labelledby="iconName"
        role="presentation"
+       :viewBox="icons[name].viewBox"
   >
-    <title :id="iconName" lang="en">{{iconName}} icon</title>
-      <g :fill="iconColor">
-      <slot />
+      <g :fill="color">
+        <component v-bind:is="icons[name].component"></component>
       </g>
       </svg>
 </template>
@@ -16,9 +15,10 @@
 <script>
 export default {
   props: {
-    iconName: {
+    name: {
       type: String,
-      default: 'box'
+      default: 'IconClose',
+      required: true
     },
     width: {
       type: [Number, String],
@@ -28,13 +28,87 @@ export default {
       type: [Number, String],
       default: 18
     },
-    iconColor: {
+    color: {
       type: String,
       default: 'currentColor'
-    },
-    viewBox: {
-      type: String,
-      default: '0 0 18 18'
+    }
+  },
+  data () {
+    return {
+      icons: {
+        alignCenter: {
+          component: 'IconAlignCenter',
+          viewBox: '0 0 18 18'
+        },
+        alignLeft: {
+          component: 'IconAlignLeft',
+          viewBox: '0 0 18 18'
+        },
+        alignRight: {
+          component: 'IconAlignRight',
+          viewBox: '0 0 18 18'
+        },
+        arrowDropDown: {
+          component: 'IconArrowDropDown',
+          viewBox: '0 0 6 4'
+        },
+        close: {
+          component: 'IconClose',
+          viewBox: '0 0 8 8'
+        },
+        colorFill: {
+          component: 'IconColorFill',
+          viewBox: '0 0 15 18'
+        },
+        drag: {
+          component: 'IconDrag',
+          viewBox: '0 0 3 21'
+        },
+        equalizer: {
+          component: 'IconEqualizer',
+          viewBox: '0 0 18 16'
+        },
+        fontBold: {
+          component: 'IconFontBold',
+          viewBox: '0 0 11 14'
+        },
+        fontFamily: {
+          component: 'IconFontFamily',
+          viewBox: '0 0 15 17'
+        },
+        fontItalic: {
+          component: 'IconFontItalic',
+          viewBox: '0 0 12 14'
+        },
+        fontSize: {
+          component: 'IconFontSize',
+          viewBox: '0 0 20 17'
+        },
+        fontUnderline: {
+          component: 'IconFontUnderline',
+          viewBox: '0 0 12 15'
+        },
+        menu: {
+          component: 'IconMenu',
+          viewBox: '0 0 16 16'
+        },
+        platformDesktop: {
+          component: 'IconPlatformDesktop',
+          viewBox: '0 0 16 14'
+        },
+        platformLaptop: {
+          component: 'IconPlatformLaptop',
+          viewBox: '0 0 22 16'
+        },
+        platformMobile: {
+          component: 'IconPlatformMobile',
+          viewBox: '0 0 10 14'
+        },
+        plus: {
+          component: 'IconPlus',
+          viewBox: '0 0 16 16'
+        }
+      }
     }
   }
 }
--- src/components/pages/Sandbox.vue
+++ src/components/pages/Sandbox.vue
@@ -43,24 +43,24 @@
     <hr>
     Icons
     <div class="icons">
-      <icon-base name="left" iconColor="red"><icon-align-left></icon-align-left></icon-base>
-      <icon-base name="center" iconColor="green"><icon-align-center></icon-align-center></icon-base>
-      <icon-base name="right" iconColor="magenta"><icon-align-right></icon-align-right></icon-base>
-      <icon-base name="down" viewBox="0 0 6 4" width="6"><icon-arrow-drop-down></icon-arrow-drop-down></icon-base>
-      <icon-base name="close" viewBox="0 0 8 8"><icon-close></icon-close></icon-base>
-      <icon-base name="fill" viewBox="0 0 15 18"><icon-color-fill></icon-color-fill></icon-base>
-      <icon-base name="drag" viewBox="0 0 3 21"><icon-drag></icon-drag></icon-base>
-      <icon-base name="equalizer" viewBox="0 0 18 16"><icon-equalizer></icon-equalizer></icon-base>
-      <icon-base name="bold" viewBox="0 0 11 14"><icon-font-bold></icon-font-bold></icon-base>
-      <icon-base name="family" viewBox="0 0 15 17"><icon-font-family></icon-font-family></icon-base>
-      <icon-base name="italic" viewBox="0 0 12 14"><icon-font-italic></icon-font-italic></icon-base>
-      <icon-base name="size" viewBox="0 0 20 17"><icon-font-size></icon-font-size></icon-base>
-      <icon-base name="underline" viewBox="0 0 12 15"><icon-font-underline></icon-font-underline></icon-base>
-      <icon-base name="menu" viewBox="0 0 16 16"><icon-menu></icon-menu></icon-base>
-      <icon-base name="plus" viewBox="0 0 16 16"><icon-plus></icon-plus></icon-base>
-      <icon-base name="desktop" viewBox="0 0 16 14"><icon-platform-desktop></icon-platform-desktop></icon-base>
-      <icon-base name="laptop" viewBox="0 0 22 16"><icon-platform-laptop></icon-platform-laptop></icon-base>
-      <icon-base name="mobile" viewBox="0 0 10 14"><icon-platform-mobile></icon-platform-mobile></icon-base>
+      <icon-base name="alignLeft" color="red"></icon-base>
+      <icon-base name="alignRight" color="green"></icon-base>
+      <icon-base name="alignCenter" color="magenta"></icon-base>
+      <icon-base name="arrowDropDown" width="6" color="black"></icon-base>
+      <icon-base name="close"></icon-base>
+      <icon-base name="colorFill"></icon-base>
+      <icon-base name="drag"></icon-base>
+      <icon-base name="equalizer"></icon-base>
+      <icon-base name="fontBold"></icon-base>
+      <icon-base name="fontFamily"></icon-base>
+      <icon-base name="fontItalic"></icon-base>
+      <icon-base name="fontSize"></icon-base>
+      <icon-base name="fontUnderline"></icon-base>
+      <icon-base name="menu"></icon-base>
+      <icon-base name="platformDesktop"></icon-base>
+      <icon-base name="platformLaptop"></icon-base>
+      <icon-base name="platformMobile"></icon-base>
+      <icon-base name="plus"></icon-base>
     </div>
   </div>
 </template>
